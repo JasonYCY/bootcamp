34284897521ae115b83bbce11c54787faed27926
Jason Yau <jasonyau0130@gmail.com>
2025-12-02 16:07:42 +0800

    update DemoMath

--- demo-datatype/src/DemoMath.java
+++ demo-datatype/src/DemoMath.java
@@ -13,7 +13,7 @@ public class DemoMath {
     short s10 = 9;
     // short s20 = b10 + s10
     int i20 = b10 + s10;
-    System.out.println(i20);
+    System.out.println("sum of byte and short: " + i20);
 
     // calculate average score
     int englishScore = 87;
@@ -24,5 +24,29 @@ public class DemoMath {
         (double) (englishScore + mathScore + historyScore) / 3;
 
     System.out.println("averageScore: " + averageScore);
+
+    // 0.1 + 0.2 = 0.30000000000000004 (Never use double variable for Math Operations -> use BigDecimal)
+    System.out.println("0.1 + 0.2 = " + (0.1 + 0.2));
+
+
+    // Overflow
+    byte overflowByte = 127;
+    overflowByte += 1;
+    System.out.println("overflowByte: " + overflowByte);
+
+    int i10 = 2_100_000_000;
+    long l10 = i10 + 100_000_000L; // int + long -> long (no overflow)
+    System.out.println("int + long -(auto cast int to long)-> long: " + l10);
+
+    // cast char into int | cast int into char
+    int a = 'a';
+    System.out.println("cast char 'a' into int: " + a + "\n");
+
+    System.out.println(
+        "cast int (65620 - 65535) into char: " + (char) (65620 - 65535));
+    System.out.println("cast int 65620 into char: " + (char) 65620);
+
+    int zeroAsciiCode = '0';
+    System.out.println("cast char '0' into int: " + zeroAsciiCode);
   }
 }
