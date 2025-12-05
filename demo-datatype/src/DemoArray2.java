c537038b7ba9a6a4df64f454780a21a77e09966e
Jason Yau <jasonyau0130@gmail.com>
2025-12-05 12:13:46 +0800

    object reference example and basic algorithm practice

---
+++ demo-datatype/src/DemoArray2.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class DemoArray2 {
+  public static void main(String[] args) {
+    /*
+     * Array - demo the idea of object reference
+     */
+    // String name1 = "John";
+    // String name2 = "Cathy";
+    // String name3 = "Jenny";
+
+    // String[] students = new String[3];
+    // students[0] = name1;
+    // students[1] = name2;
+    // students[2] = name3;
+
+    // name1 = "Leo";
+    // System.out.println(students[0]);
+
+    /*
+     * Algorithm
+     */
+    // 1) Swap
+    // int a = 10;
+    // int b = 20;
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // 2) Find the highest score and the lowest score
+    // int[] scores = new int[] {23, 8, 97, 56};
+    // int highestScore = scores[0];
+    // int lowestScore = scores[0];
+    // for (int i = 1; i < scores.length; i++) {
+    // if (scores[i] > highestScore) {
+    // highestScore = scores[i];
+    // }
+    // if (scores[i] < lowestScore) {
+    // lowestScore = scores[i];
+    // }
+    // }
+    // System.out.println("Array: " + Arrays.toString(scores));
+    // System.out.println("Highest score: " + highestScore);
+    // System.out.println("Lowest score: " + lowestScore);
+
+    // 3) Move the max value to the tail
+    int[] scores = new int[] {23, 100, 8, 97, 56};
+    System.out.println("Before: " + Arrays.toString(scores));
+    for (int i = 0; i < scores.length - 1; i++) {
+      if (scores[i] > scores[i + 1]) {
+        int temp = scores[i + 1];
+        scores[i + 1] = scores[i];
+        scores[i] = temp;
+      }
+    }
+    System.out.println("After: " + Arrays.toString(scores));
+
+
+
+  }
+}
