42e832d908a720b57a9aee5ad37f9cc937c77cfa
Jason Yau <jasonyau0130@gmail.com>
2025-12-15 13:05:12 +0800

    Dog and Cat inherit Animal

--- demo-apie/src/Animal.java
+++ demo-apie/src/Animal.java
@@ -1,3 +1,12 @@
 public abstract class Animal {
-  
+  private String name;
+  private int age;
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
 }
