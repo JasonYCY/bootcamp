3ca216b082bd838a79684d8349c0326c76b5e282
Jason Yau <jasonyau0130@gmail.com>
2025-12-16 12:49:43 +0800

    interface explanation

--- demo-apie/src/Eatable.java
+++ demo-apie/src/Eatable.java
@@ -1,3 +1,9 @@
+// 1. No attributes
+// 2. Methods are abstract by default
+// 3. No constructor
+// 4. Variable are static by default
+// 5. Can define static method
+// 6. Can have default method
 public interface Eatable {
   void eat();
 }
