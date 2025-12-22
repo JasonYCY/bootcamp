d9c0d9e22630c6a8fc228c680d32496ba78df876
Jason Yau <jasonyau0130@gmail.com>
2025-12-22 10:39:02 +0800

    student array list

--- demo-data-structure/src/DemoArrayList.java
+++ demo-data-structure/src/DemoArrayList.java
@@ -16,5 +16,14 @@ public class DemoArrayList {
     // remove() -> break the for loop when it found the element
     names.remove(new String("Mary"));
     System.out.println(names);
+
+    Student s1 = new Student("John", 83);
+    ArrayList<Student> students = new ArrayList<>();
+    students.add(s1);
+    students.add(new Student("Steve", 25));
+    students.add(new Student("Mary", 100));
+
+    s1.setScore(90);
+    System.out.println(students.get(0).getScore());
   }
 }
