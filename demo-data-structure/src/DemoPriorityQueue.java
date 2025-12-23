eee8c577b3303c6227b6bd342b742528d388742a
Jason Yau <jasonyau0130@gmail.com>
2025-12-23 16:57:14 +0800

    Demo Priority Queue

---
+++ demo-data-structure/src/DemoPriorityQueue.java
@@ -0,0 +1,8 @@
+import java.util.PriorityQueue;
+
+public class DemoPriorityQueue {
+  public static void main(String[] args) {
+    PriorityQueue<String> names = new PriorityQueue<>();
+    System.out.println(names);
+  }
+}
