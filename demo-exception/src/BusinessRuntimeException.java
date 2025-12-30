0615c89983fd13c8831d11ab3461ed52e98861c5
Jason Yau <jasonyau0130@gmail.com>
2025-12-30 11:27:20 +0800

    Custom RuntimeException demo

---
+++ demo-exception/src/BusinessRuntimeException.java
@@ -0,0 +1,38 @@
+public class BusinessRuntimeException extends RuntimeException {
+  private int code;
+
+  private BusinessRuntimeException(SysCode sysCode) {
+    super(sysCode.getMessage());
+    this.code = sysCode.getCode();
+  }
+
+  public static BusinessRuntimeException of(SysCode sysCode) {
+    return new BusinessRuntimeException(sysCode);
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public static class ArrayIndexOutOfRange extends RuntimeException {}
+
+
+  // main method
+  public static void main(String[] args) {
+    // test
+    try {
+      int[] arr = new int[3];
+      int result = arr[-1];
+      System.out.println(result);
+    } catch (RuntimeException e) {
+      // Re-throw
+      if (e instanceof ArrayIndexOutOfBoundsException) {
+        throw new ArrayIndexOutOfRange();
+      }
+    }
+    
+
+
+
+  }
+}
