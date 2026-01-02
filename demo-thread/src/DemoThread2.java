8a574a568d160a73c78f1f1b19f056f63533ed78
Jason Yau <jasonyau0130@gmail.com>
2026-01-02 12:48:15 +0800

    Demo vector & array list time difference

--- demo-thread/src/DemoThread2.java
+++ demo-thread/src/DemoThread2.java
@@ -1,9 +1,44 @@
 // import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class DemoThread2 {
   public static void main(String[] args) {
+    // ArrayList vs Vector
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    Vector<Integer> vector = new Vector<>();
+
+    // Declaration
+    long startTime;
+    long endTime;
+    long arrayListTime;
+    long vectorTime;
+    int size = 1_000_000;
+
+    // ArrayList
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      arrayList.add(i);
+    }
+    endTime = System.currentTimeMillis();
+    arrayListTime = endTime - startTime;
+    
+    // Vector
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < size; i++) {
+      vector.add(i);
+    }
+    endTime = System.currentTimeMillis();
+    vectorTime = endTime - startTime;
+
+    // Print Time
+    System.out.println("ArrayList used: " + arrayListTime + "ms");
+    System.out.println("Vector used: " + vectorTime + "ms");
+    System.out.println();
+
+
+
     // List<Integer> integers = new ArrayList<>();
     List<Integer> integers = new Vector<>();
 
@@ -36,5 +71,10 @@ public class DemoThread2 {
     // < 200_000
     System.out.println("Size of integers: " + integers.size());
     System.out.println("Time used: " + (after - before) + "ms");
+
+
+
+
+    
   }
 }
