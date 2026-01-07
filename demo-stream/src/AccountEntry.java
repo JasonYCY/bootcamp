eabb6ceef3255f082d3ba7d84c66a80e9962abe0
Jason Yau <jasonyau0130@gmail.com>
2026-01-07 17:48:46 +0800

    stream API with List of record

--- demo-stream/src/AccountEntry.java
+++ demo-stream/src/AccountEntry.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public record AccountEntry(Action action, Currency currency, double amount) {
   // inner enum definition
   public enum Action {
@@ -46,11 +51,34 @@ public record AccountEntry(Action action, Currency currency, double amount) {
 
   // main method
   public static void main(String[] args) {
-    AccountEntry entry = new AccountEntry(Action.CREDIT, Currency.USD, 100);
-    System.out.println(entry);
-    System.out.println(entry.action());
-    System.out.println(entry.currency());
-    System.out.println(entry.amount());
+    // AccountEntry entry = new AccountEntry(Action.CREDIT, Currency.USD, 100);
+    // System.out.println(entry);
+    // System.out.println(entry.action());
+    // System.out.println(entry.currency());
+    // System.out.println(entry.amount());
+
+    List<AccountEntry> entries = new ArrayList<>(
+      List.of(
+        new AccountEntry(Action.CREDIT, Currency.USD, 100),
+        new AccountEntry(Action.CREDIT, Currency.USD, 666),
+
+        new AccountEntry(Action.DEBIT, Currency.USD, 90),
+        new AccountEntry(Action.DEBIT, Currency.USD, 70)
+      )
+    );
+    entries.forEach(System.out::println);
+    System.out.println();
+
+    Map<String, List<AccountEntry>> test = entries.stream()
+      .collect(Collectors.groupingBy(entry -> entry.action().name()));
+
+    test.forEach((key, value) -> {
+      System.out.print("Key:");
+      System.out.println("\t" + key);
+
+      System.out.print("Value:");
+      value.forEach(entry -> System.out.println("\t" + entry));
+    });
 
 
 
